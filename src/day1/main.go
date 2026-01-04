03d1001fc61cbff4ed4a0c0192cca9f13544ffb2
Sunil <iamsunilsharma999@gmail.com>
2026-01-05 01:13:29 +0530

    day 1: part 1

--- src/day1/main.go
+++ src/day1/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello world from day1")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	rotations := []string{}
+
+	for scanner.Scan() {
+		rotations = append(rotations, scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		panic("error reading standard input")
+	}
+	start := 50
+	count := 0
+	for _, line := range rotations {
+		dir := line[0:1]
+		distance, _ := strconv.Atoi(line[1:])
+
+		switch dir {
+		case "R":
+			start = (((start + distance) % 100) + 100) % 100
+		case "L":
+			start = (((start - distance) % 100) + 100) % 100
+		default:
+			panic("NOOOOOOOOO!!!")
+		}
+		if start == 0 {
+			count++
+		}
+	}
+	fmt.Println("Part 1: What's the actual password to open the door?", count)
 }
