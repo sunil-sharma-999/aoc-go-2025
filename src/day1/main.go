861628eb1fb6a86a4c55910d6f9949aa4807c048
Sunil <iamsunilsharma999@gmail.com>
2026-01-08 17:55:06 +0530

    day 2

--- src/day1/main.go
+++ src/day1/main.go
@@ -20,22 +20,32 @@ func main() {
 		panic("error reading standard input")
 	}
 	start := 50
-	count := 0
+	part1C := 0
+	part2C := 0
 	for _, line := range rotations {
 		dir := line[0:1]
 		distance, _ := strconv.Atoi(line[1:])
 
 		switch dir {
-		case "R":
-			start = (((start + distance) % 100) + 100) % 100
 		case "L":
-			start = (((start - distance) % 100) + 100) % 100
-		default:
-			panic("NOOOOOOOOO!!!")
+			for range distance {
+				start = (start - 1) % 100
+				if start == 0 {
+					part2C++
+				}
+			}
+		case "R":
+			for range distance {
+				start = (start + 1) % 100
+				if start == 0 {
+					part2C++
+				}
+			}
 		}
 		if start == 0 {
-			count++
+			part1C++
 		}
 	}
-	fmt.Println("Part 1: What's the actual password to open the door?", count)
+	fmt.Println("Part 1: What's the actual password to open the door?", part1C)
+	fmt.Println("Part 2: what is the password to open the door?", part2C)
 }
