c06d3bd21e1db4b303eb9973c403322cfe06d643
Sunil <iamsunilsharma999@gmail.com>
2026-04-12 00:33:56 +0530

    day 3: part 2

--- src/day3/main.go
+++ src/day3/main.go
@@ -7,32 +7,40 @@ import (
 	"github.com/sunil-sharma-999/aoc-go-2025/src/utils"
 )
 
+func outputJoltage(bank string, joltageLen int) int {
+	maxIndex := -1
+	var max rune
+	var numStr string
+	for cell := range joltageLen {
+		s := maxIndex + 1
+		e := len(bank) - (joltageLen - cell) + 1
+		for index, char := range bank[s:e] {
+			if char > max {
+				max = char
+				maxIndex = s + index
+			}
+		}
+		numStr = numStr + string(max)
+		max = 0
+	}
+	num, _ := strconv.Atoi(numStr)
+
+	return num
+}
+
 func main() {
 	banks := utils.ReadLines()
 	defer utils.Track()()
 
 	part1 := 0
+	part2 := 0
 
 	for _, bank := range banks {
-		maxIndex := -1
-		var max rune
-		for i, char := range bank[0 : len(bank)-1] {
-			if char > max {
-				max = char
-				maxIndex = i
-			}
-		}
-		var max2nd rune
-		for _, char := range bank[maxIndex+1:] {
-			if char > max2nd {
-				max2nd = char
-			}
-		}
-		numStr := string(max) + string(max2nd)
-		num, _ := strconv.Atoi(numStr)
-		part1 += num
+		part1 += outputJoltage(bank, 2)
+		part2 += outputJoltage(bank, 12)
 	}
 
-	fmt.Println("What do you get if you add up all of the invalid IDs?", part1)
+	fmt.Println("what is the total output joltage?", part1)
+	fmt.Println("What is the new total output joltage?", part2)
 
 }
